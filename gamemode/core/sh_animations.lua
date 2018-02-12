ca9886b96c5aff522037eab8e084017fcc4eef13
Jake Green <jaketg10@gmail.com>
2018-02-12 15:14:30 +0000

    Animations from IMPULSE

---
+++ gamemode/core/sh_animations.lua
@@ -0,0 +1,17 @@
+local IsValid = IsValid
+local os = os
+local util = util
+hook.Add("CalcMainActivity", "GFL-ANIMATIONBASE", function( Player, Velocity )
+	local speed = Velocity:Length()
+
+	if Player:IsOnGround() and not Player:Crouching() and not Player:InVehicle() then
+		if speed <= Player:GetWalkSpeed() + 10 then
+			return ACT_HL2MP_WALK, -1
+		elseif speed > 290 then
+			return ACT_HL2MP_RUN_FAST, -1
+		elseif speed > Player:GetRunSpeed() - 10 then
+			return ACT_MP_RUN, -1
+		end
+	end
+
+end)
